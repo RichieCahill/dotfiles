76d67502e6de6b6a62ed73d628b5005dbce94317
Richie Cahill <Richie@tmmworkshop.com>
2024-09-14 08:54:10 -0400

    removed useDHCP from jeeves

--- systems/jeeves/hardware.nix
+++ systems/jeeves/hardware.nix
@@ -53,10 +53,6 @@
 
   swapDevices = [ ];
 
-  networking.useDHCP = lib.mkDefault true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-
 }
