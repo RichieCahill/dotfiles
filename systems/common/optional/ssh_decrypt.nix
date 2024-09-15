a5f09c1df7979fabeae0088a029048f656943c05
Richie Cahill <Richie@tmmworkshop.com>
2024-09-15 11:57:36 -0400

    fixed mistake

--- systems/common/optional/ssh_decrypt.nix
+++ systems/common/optional/ssh_decrypt.nix
@@ -1,13 +1,15 @@
 { config, ... }:
 {
-  boot.initrd.network = {
-    enable = true;
-    ssh = {
+  boot.initrd = {
+    network = {
       enable = true;
-      port = 2222;
-      hostKeys = [ "/etc/ssh/initrd_ssh_host_ed25519_key" ];
-      authorizedKeys = config.users.users.richie.openssh.authorizedKeys.keys;
+      ssh = {
+        enable = true;
+        port = 2222;
+        hostKeys = [ "/etc/ssh/initrd_ssh_host_ed25519_key" ];
+        authorizedKeys = config.users.users.richie.openssh.authorizedKeys.keys;
+      };
     };
+    availableKernelModules = [ "igb" ];
   };
-  availableKernelModules = [ "igb" ];
 }
