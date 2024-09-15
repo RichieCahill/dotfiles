0c8d72aa303e09c4507501e525bd365ad6385cf1
Richie Cahill <Richie@tmmworkshop.com>
2024-09-15 10:36:13 -0400

    fixed typo

--- systems/jeeves/docker/internal.nix
+++ systems/jeeves/docker/internal.nix
@@ -52,7 +52,7 @@ in
         DELUGE_DAEMON_LOG_LEVEL = "debug";
         DELUGE_WEB_LOG_LEVEL = "debug";
       };
-      environmentFiles = [/root/secrets/docker/qbit_vpn];
+      environmentFiles = [/root/secrets/docker/qbitvpn];
       autoStart = true;
     };
     bazarr = {
