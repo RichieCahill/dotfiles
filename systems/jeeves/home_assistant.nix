5d8141736d905ac8779f66d2fe2b9cf19545040d
Richie Cahill <Richie@tmmworkshop.com>
2024-09-19 20:15:50 -0400

    created home_assistant.nix for jeeves

---
+++ systems/jeeves/home_assistant.nix
@@ -0,0 +1,49 @@
+{
+  services.home-assistant = {
+    enable = true;
+    openFirewall = true;
+    config = {
+      http = {
+        server_port = 8123;
+        server_host = [
+          "192.168.95.14"
+          "192.168.90.40"
+          "192.168.98.4"
+        ];
+        use_x_forwarded_for = true;
+        trusted_proxies = "172.100.0.4";
+      };
+      homeassistant = {
+        time_zone = "America/New_York";
+        unit_system = "imperial";
+        temperature_unit = "F";
+      };
+      assist_pipeline = { };
+      backup = { };
+      bluetooth = { };
+      config = { };
+      dhcp = { };
+      energy = { };
+      history = { };
+      homeassistant_alerts = { };
+      image_upload = { };
+      logbook = { };
+      media_source = { };
+      mobile_app = { };
+      ssdp = { };
+      sun = { };
+      webhook = { };
+      zeroconf = { };
+    };
+    extraPackages =
+      python3Packages: with python3Packages; [
+        psycopg2
+        gtts
+        aioesphomeapi
+        esphome-dashboard-api
+        bleak-esphome
+        pymetno
+      ];
+    extraComponents = [ "isal" ];
+  };
+}
