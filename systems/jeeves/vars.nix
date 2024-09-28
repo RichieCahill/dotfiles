c749bf3f127dae7453a8c0158500553afd2ad40a
Richie Cahill <Richie@tmmworkshop.com>
2024-09-28 12:14:01 -0400

    updated vars

--- systems/jeeves/vars.nix
+++ systems/jeeves/vars.nix
@@ -6,13 +6,12 @@ in
 {
   inherit zfs_media zfs_storage zfs_torrenting;
   # media
-  media_database = "${zfs_media}/syncthing/database";
+  media_database = "${zfs_media}/database";
   media_docker = "${zfs_media}/docker";
   media_docker_configs = "${zfs_media}/docker/configs";
   media_mirror = "${zfs_media}/mirror";
   media_notes = "${zfs_media}/notes";
-  media_plex = "${zfs_media}/plex/";
-  media_scripts = "${zfs_media}/scripts";
+  media_plex = "${zfs_media}/plex";
   # storage
   storage_main = "${zfs_storage}/main";
   storage_plex = "${zfs_storage}/plex";
