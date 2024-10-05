6700faff4396ba899995ed35006c1de9351c9b5a
Richie Cahill <Richie@tmmworkshop.com>
2024-10-04 21:24:19 -0400

    fixed syncthing

--- systems/common/optional/syncthing_base.nix
+++ systems/common/optional/syncthing_base.nix
@@ -12,6 +12,7 @@
         jeeves.id = "RCDU465-AIQRBEJ-VWC4EZF-2AMXABC-F3S4NFW-QA4ZUAQ-OVNUBLI-BUJJTA2"; # cspell:disable-line
         ipad.id = "KI76T3X-SFUGV2L-VSNYTKR-TSIUV5L-SHWD3HE-GQRGRCN-GY4UFMD-CW6Z6AX"; # cspell:disable-line
         bob.id = "CJIAPEJ-VO74RR4-F75VU6M-QNZAMYG-FYUJG7Y-6AT62HJ-355PRPL-PJFETAZ"; # cspell:disable-line
+        rhapsody-in-green.id = "ASL3KC4-3XEN6PA-7BQBRKE-A7JXLI6-DJT43BY-Q4WPOER-7UALUAZ-VTPQ6Q4"; # cspell:disable-line
       };
     };
   };
