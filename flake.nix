9869f88a3efd3c801c31becd14e9e88718efae0b
Richie Cahill <Richie@tmmworkshop.com>
2024-11-02 21:26:42 -0400

    added nixpkgs-master

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-master.url = "github:nixos/nixpkgs/master";
     systems.url = "github:nix-systems/default-linux";
 
     nixos-hardware.url = "github:nixos/nixos-hardware/master";
--- overlays/default.nix
+++ overlays/default.nix
@@ -6,4 +6,11 @@
       config.allowUnfree = true;
     };
   };
+  # When applied, the master nixpkgs set (declared in the flake inputs) will be accessible through 'pkgs.master'
+  master = final: _prev: {
+    master = import inputs.nixpkgs-master {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  };
 }
