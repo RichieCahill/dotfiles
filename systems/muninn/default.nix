b8e46ee3415ff3ab770cf1ef3617544b436752ea
Richie Cahill <Richie@tmmworkshop.com>
2024-11-17 12:09:54 -0500

    test

--- systems/muninn/default.nix
+++ systems/muninn/default.nix
@@ -1,8 +1,8 @@
-{ pkgs, ... }:
 {
   imports = [
     ../../users/richie
     ../../common/global
+    ../../common/optional/desktop.nix
     ../../common/optional/steam.nix
     ../../common/optional/systemd-boot.nix
     ../../common/optional/update.nix
@@ -26,15 +26,7 @@
 
   security.rtkit.enable = true;
 
-  boot = {
-    kernelPackages = pkgs.linuxPackages_zen;
-    zfs.package = pkgs.zfs_unstable;
-  };
-
-  programs.sway.enable = true;
-
   services = {
-    desktopManager.plasma6.enable = true;
     displayManager = {
       enable = true;
       autoLogin.user = "richie";
