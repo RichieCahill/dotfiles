a318ee74fef21b69150e0cb2c8f5c69117627a3b
Richie Cahill <Richie@tmmworkshop.com>
2024-11-21 20:24:11 -0500

    enabled postgres

--- systems/jeeves/docker/postgresql.nix
+++ systems/jeeves/docker/postgresql.nix
@@ -13,20 +13,20 @@ in
     };
   };
 
-  # virtualisation.oci-containers.containers = {
-  #   postgres = {
-  #     image = "postgres:16";
-  #     ports = [ "5432:5432" ];
-  #     volumes = [ "${vars.media_database}/postgres:/var/lib/postgresql/data" ];
-  #     environment = {
-  #       POSTGRES_USER = "admin";
-  #       POSTGRES_DB = "archive";
-  #       POSTGRES_INITDB_ARGS = "--auth-host=scram-sha-256";
-  #     };
-  #     environmentFiles = [/root/secrets/docker/postgres];
-  #     autoStart = true;
-  #     user = "postgres:postgres";
-  #   };
-  # };
+  virtualisation.oci-containers.containers = {
+    postgres = {
+      image = "postgres:17";
+      ports = [ "5432:5432" ];
+      volumes = [ "${vars.media_database}/postgres:/var/lib/postgresql/data" ];
+      environment = {
+        POSTGRES_USER = "admin";
+        POSTGRES_DB = "archive";
+        POSTGRES_INITDB_ARGS = "--auth-host=scram-sha-256";
+      };
+      environmentFiles = [/root/secrets/docker/postgres];
+      autoStart = true;
+      user = "postgres:postgres";
+    };
+  };
 
 }
