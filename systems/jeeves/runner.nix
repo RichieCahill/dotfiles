d91a7131dbddd1d7a4099a129a94fe0d0149c1a6
Richie Cahill <Richie@tmmworkshop.com>
2024-11-25 00:22:56 -0500

    added github-runners user and group

--- systems/jeeves/runner.nix
+++ systems/jeeves/runner.nix
@@ -2,12 +2,25 @@ let
   vars = import ./vars.nix;
 in
 {
+  users = {
+    users.github-runners = {
+      isSystemUser = true;
+      group = "github-runners";
+      uid = 601;
+    };
+    groups.github-runners = {
+      gid = 601;
+    };
+  };
+  
   services.github-runners.nix_builder = {
     enable = true;
     workDir = "/home/richie/test";
     url = "https://github.com/RichieCahill/dotfiles";
     extraLabels = [ "nixos" ];
     tokenFile = "${vars.storage_secrets}/services/github_runners/nix_builder";
+    user = "github-runners";
+    group = "github-runners";
     # extraPackages
     # extraEnvironment
   };
