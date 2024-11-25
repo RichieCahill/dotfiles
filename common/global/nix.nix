234d32b6783f2c0e9d8e2482ab6cd8a7d7b73717
Richie Cahill <Richie@tmmworkshop.com>
2024-11-25 15:11:49 -0500

    adding substituters to nix settings

--- common/global/nix.nix
+++ common/global/nix.nix
@@ -11,6 +11,21 @@ in {
         "root"
         "@wheel"
       ];
+      trusted-substituters = [
+        "https://cache.nixos.org"
+        "https://cache.tmmworkshop.com"
+        "https://nix-community.cachix.org"
+      ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://cache.tmmworkshop.com"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cache.tmmworkshop.com:jHffkpgbmEdstQPoihJPYW9TQe6jnQbWR2LqkNGV3iA="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
       auto-optimise-store = lib.mkDefault true;
       experimental-features = [
         "nix-command"
