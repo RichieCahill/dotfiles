03a0f4ead73f59e45197e20003fa4bfdbe18c504
Richie Cahill <Richie@tmmworkshop.com>
2024-11-25 14:44:44 -0500

    adding nixos-rebuild to extraPackages

--- systems/jeeves/runner.nix
+++ systems/jeeves/runner.nix
@@ -16,7 +16,8 @@ in
   
   services.github-runners.nix_builder = {
     enable = true;
-    workDir = "/home/richie/test";
+    replace = true;
+    workDir = "/zfs/media/github-runners/nix_builder/";
     url = "https://github.com/RichieCahill/dotfiles";
     extraLabels = [ "nixos" ];
     tokenFile = "${vars.storage_secrets}/services/github_runners/nix_builder";
