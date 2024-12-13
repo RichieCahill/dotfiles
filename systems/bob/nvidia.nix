60478dd979282376fea540bf24d5ea1d90c85e1c
Richie Cahill <Richie@tmmworkshop.com>
2024-12-12 20:42:46 -0500

    moving to nvidiaPackages.beta

--- systems/bob/nvidia.nix
+++ systems/bob/nvidia.nix
@@ -5,8 +5,9 @@
     nvidia = {
       modesetting.enable = true;
       powerManagement.enable = true;
-      package = config.boot.kernelPackages.nvidiaPackages.production;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
       nvidiaSettings = true;
+      open = true;
     };
     nvidia-container-toolkit.enable = true;
   };
