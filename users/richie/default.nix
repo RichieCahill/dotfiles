1770cb87d3023ad489315bbab3ac1dd3ecb6ff13
Richie Cahill <Richie@tmmworkshop.com>
2025-01-16 22:38:47 -0500

    removed bob authorizedKey

--- users/richie/default.nix
+++ users/richie/default.nix
@@ -19,7 +19,6 @@ in {
     shell = pkgs.zsh;
     group = "richie";
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIPtuYhiJHRTYhNaDmTcJOqJASk7D8mIn6u3F1IN5AFJ bob" # cspell:disable-line
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYZFsc9CSH03ZUP7y81AHwSyjLwFmcshVFCyxDcYhBT rhapsody-in-green" # cspell:disable-line
     ];
     extraGroups =
