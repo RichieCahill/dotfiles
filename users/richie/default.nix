ead5fc36d825b7493b5522df2b8cb7344549a9b9
Richie Cahill <Richie@tmmworkshop.com>
2025-01-16 22:39:44 -0500

    reformatted richie default.nix

--- users/richie/default.nix
+++ users/richie/default.nix
@@ -11,39 +11,39 @@ in {
     neededForUsers = true;
   };
 
-  users.users.richie = {
-    isNormalUser = true;
+  users = {
+    users.richie = {
+      isNormalUser = true;
 
-    hashedPasswordFile = "${config.sops.secrets.richie_password.path}";
+      hashedPasswordFile = "${config.sops.secrets.richie_password.path}";
 
-    shell = pkgs.zsh;
-    group = "richie";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYZFsc9CSH03ZUP7y81AHwSyjLwFmcshVFCyxDcYhBT rhapsody-in-green" # cspell:disable-line
-    ];
-    extraGroups =
-    [
-      "audio"
-      "video"
-      "wheel"
-      "users"
-    ]
-    ++ ifTheyExist [
-      "dialout"
-      "docker"
-      "hass"
-      "libvirtd"
-      "networkmanager"
-      "plugdev"
-      "scanner"
-      "uaccess"
-      "wireshark"
-    ];
-    uid = 1000;
-  };
+      shell = pkgs.zsh;
+      group = "richie";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYZFsc9CSH03ZUP7y81AHwSyjLwFmcshVFCyxDcYhBT rhapsody-in-green" # cspell:disable-line
+      ];
+      extraGroups =
+      [
+        "audio"
+        "video"
+        "wheel"
+        "users"
+      ]
+      ++ ifTheyExist [
+        "dialout"
+        "docker"
+        "hass"
+        "libvirtd"
+        "networkmanager"
+        "plugdev"
+        "scanner"
+        "uaccess"
+        "wireshark"
+      ];
+      uid = 1000;
+    };
 
-  users.groups.richie = {
-    gid = 1000;
+    groups.richie.gid = 1000;
   };
 
   home-manager.users.richie = import ./systems/${config.networking.hostName}.nix; 
