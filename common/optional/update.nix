2d3cbbf80606d04961cd42c99b6e56a5d682d276
Richie Cahill <Richie@tmmworkshop.com>
2025-01-19 14:38:24 -0500

    added update.nix to bob

--- common/optional/update.nix
+++ common/optional/update.nix
@@ -1,32 +1,9 @@
-{  lib, pkgs, ... }:
 {
-  systemd = {
-    services."autopull@dotfiles" = {
-      requires = [ "multi-user.target" ];
-      after = [ "multi-user.target" ];
-      description = "Pull the latest data for dotfiles";
-      serviceConfig = {
-        Type = "oneshot";
-        User = "root";
-        WorkingDirectory = /root/dotfiles;
-        ExecStart = "${pkgs.git}/bin/git pull --all --prune";
-      };
-    };
-    timers."autopull@dotfiles" = {
-      wantedBy = [ "timers.target" ];
-      timerConfig = {
-        OnBootSec = "1h";
-        OnUnitActiveSec = "1h";
-        Unit = "autopull@dotfiles.service";
-      };
-    };
-  };
-
   system.autoUpgrade = {
-    enable = lib.mkDefault true;
+    enable = true;
     flags = [ "--accept-flake-config" ];
     randomizedDelaySec = "1h";
     persistent = true;
-    flake = "github:RAD-Development/nix-dotfiles";
+    flake = "github:RichieCahill/dotfiles";
   };
 }
