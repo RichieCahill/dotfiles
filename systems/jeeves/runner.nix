8f2496d67641f3f4d106afc38912b2d9cb13cdef
Richie Cahill <Richie@tmmworkshop.com>
2025-01-20 12:50:46 -0500

    created nix_builder module

--- systems/jeeves/runner.nix
+++ systems/jeeves/runner.nix
@@ -1,8 +1,8 @@
-{ pkgs, ... }:
-let
-  vars = import ./vars.nix;
-in
 {
+  imports = [
+    ./nix_builder.nix
+  ];
+
   users = {
     users.github-runners = {
       isSystemUser = true;
@@ -12,49 +12,10 @@ in
     groups.github-runners.gid = 601;
   };
   
-  services.github-runners.nix_builder = {
-    enable = true;
-    replace = true;
-    workDir = "/zfs/media/github-runners/nix_builder/";
-    url = "https://github.com/RichieCahill/dotfiles";
-    extraLabels = [ "nixos" ];
-    tokenFile = "${vars.storage_secrets}/services/github_runners/nix_builder";
-    user = "github-runners";
-    group = "github-runners";
-    extraPackages = [ pkgs.nixos-rebuild ];
-    # extraEnvironment
-  };
 
-  containers.nix-builder-1 = {
-    autoStart = true;
-    bindMounts = {
-      "/test" = {
-        mountPoint = "/zfs/media/github-runners/nix_builder_1";
-        isReadOnly = false;
-      };
-      "/secrets".mountPoint = "${vars.storage_secrets}/services/github_runners/nix_builder_1";
-    };
-    config = { config, pkgs, lib, ... }: {
-      users = {
-        users.github-runners = {
-          isSystemUser = true;
-          group = "github-runners";
-          uid = 601;
-        };
-        groups.github-runners.gid = 601;
-      };
-      services.github-runners.nix_builder_1 = {
-        enable = true;
-        replace = true;
-        workDir = "/zfs/media/github-runners/nix_builder_1";
-        url = "https://github.com/RichieCahill/dotfiles";
-        extraLabels = [ "nixos" ];
-        tokenFile = "${vars.storage_secrets}/services/github_runners/nix_builder_1";
-        user = "github-runners";
-        group = "github-runners";
-        extraPackages = [ pkgs.nixos-rebuild ];
-      };
-      system.stateVersion = "24.11";
-    };
+  services.nix_builder.containers = {
+    nix-builder-0.enable = true;
+    nix-builder-1.enable = true;
+    nix-builder-2.enable = true;
   };
 }
