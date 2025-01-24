50942f3e8c659a252eafe38f2d3010221e370913
Richie Cahill <Richie@tmmworkshop.com>
2025-01-23 19:07:06 -0500

    added nix setting to nix_builder containers

--- systems/jeeves/nix_builder.nix
+++ systems/jeeves/nix_builder.nix
@@ -26,6 +26,27 @@ in
         "ssh-keys".mountPoint = "${vars.storage_secrets}/services/github-runners/id_ed25519_github-runners";
       };
       config = { config, pkgs, lib, ... }: {
+          nix.settings = {
+            trusted-substituters = [
+              "https://cache.nixos.org"
+              "https://cache.tmmworkshop.com"
+              "https://nix-community.cachix.org"
+            ];
+            substituters = [
+              "https://cache.nixos.org/?priority=2&want-mass-query=true"
+              "https://cache.tmmworkshop.com/?priority=2&want-mass-query=true"
+              "https://nix-community.cachix.org/?priority=10&want-mass-query=true"
+            ];
+            trusted-public-keys = [
+              "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+              "cache.tmmworkshop.com:jHffkpgbmEdstQPoihJPYW9TQe6jnQbWR2LqkNGV3iA="
+              "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            ];
+            experimental-features = [
+              "flakes"
+              "nix-command"
+            ];
+          };
           programs.ssh.extraConfig = ''
             Host jeeves
               Port 629
