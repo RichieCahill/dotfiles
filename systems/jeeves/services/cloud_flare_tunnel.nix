60c3e3db208bf602f09529e2a1e0e6be0fdea81a
Richie Cahill <Richie@tmmworkshop.com>
2025-02-03 12:31:38 -0500

    moved cloud_flare_tunnel to a service

---
+++ systems/jeeves/services/cloud_flare_tunnel.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+let
+  vars = import ../vars.nix;
+in
+{
+  systemd.services.cloud_flare_tunnel = {
+    description = "cloud_flare_tunnel proxy's traffic through cloudflare";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "simple";
+      EnvironmentFile = "${vars.storage_secrets}/docker/cloud_flare_tunnel";
+      ExecStart = "${pkgs.cloudflared}/bin/cloudflared --no-autoupdate tunnel run";
+      Restart = "on-failure";
+    };
+  };
+}
+
