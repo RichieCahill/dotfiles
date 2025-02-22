bdf9d267e8320fc8cd1c0fa11b095c94ae883efc
Richie Cahill <Richie@tmmworkshop.com>
2025-02-21 20:31:34 -0500

    moved models

--- systems/bob/llms.nix
+++ systems/bob/llms.nix
@@ -4,14 +4,15 @@
       enable = true;
       loadModels = [
         "codellama:7b"
-        "deepseek-r1:1.5b"
-        "deepseek-r1:7b"
         "deepseek-r1:8b"
         "deepseek-r1:14b"
         "deepseek-r1:32b"
         "llama3.2:3b"
+        "llama2-uncensored:7b"
         "mistral-nemo:12b"
+        "dolphin-mixtral:8x7b"
       ];
+      models = "/zfs/models";
       acceleration = "cuda";
       openFirewall = true;
     };
