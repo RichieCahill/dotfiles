8576cb21135bb37f5ab787c8539045d3b30a6cf2
Richie Cahill <Richie@tmmworkshop.com>
2025-03-23 10:40:37 -0400

    updated phone id

--- common/optional/syncthing_base.nix
+++ common/optional/syncthing_base.nix
@@ -8,7 +8,7 @@
     dataDir = "/home/richie/Syncthing";
     configDir = "/home/richie/.config/syncthing";
     settings.devices = {
-      phone.id = "LTGPLAE-M4ZDJTM-TZ3DJGY-SLLAVWF-CQDVEVS-RGCS75T-GAPZYK3-KUM6LA5"; # cspell:disable-line
+      phone.id = "TBRULKD-7DZPGGZ-F6LLB7J-MSO54AY-7KLPBIN-QOFK6PX-W2HBEWI-PHM2CQI"; # cspell:disable-line
       jeeves.id = "ICRHXZW-ECYJCUZ-I4CZ64R-3XRK7CG-LL2HAAK-FGOHD22-BQA4AI6-5OAL6AG"; # cspell:disable-line
       ipad.id = "KI76T3X-SFUGV2L-VSNYTKR-TSIUV5L-SHWD3HE-GQRGRCN-GY4UFMD-CW6Z6AX"; # cspell:disable-line
       bob.id = "CJIAPEJ-VO74RR4-F75VU6M-QNZAMYG-FYUJG7Y-6AT62HJ-355PRPL-PJFETAZ"; # cspell:disable-line
