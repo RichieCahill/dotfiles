5f8a0fc2276f5582906262c840ec7c5aee2ef858
Richie Cahill <Richie@tmmworkshop.com>
2025-03-26 21:50:00 -0400

    fixed network settings

--- systems/jeeves/services/grafana.nix
+++ systems/jeeves/services/grafana.nix
@@ -2,8 +2,15 @@ let
   vars = import ../vars.nix;
 in
 {
+  networking.firewall.allowedTCPPorts = [
+    3000
+  ];
+
   services.grafana = {
     enable = true;
     dataDir = "${vars.media_services}/grafana";
+    settings = {
+      server.http_addr = "0.0.0.0";
+    };
   };
 }
