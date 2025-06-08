2fe3ad1f689c52b50866c87aa35e0524277a8a4f
Richie Cahill <Richie@tmmworkshop.com>
2025-06-08 15:03:57 -0400

    ran treefmt

--- users/richie/home/cli/zsh.nix
+++ users/richie/home/cli/zsh.nix
@@ -25,7 +25,8 @@
 
       "rspace" = "'for f in *\ *; do mv \"$f\" \"\${f// /_}\"; done'";
       "rebuild" = "sudo nixos-rebuild switch --flake /home/richie/dotfiles#$HOST";
-      "rebuild_backup" = "sudo nixos-rebuild switch --flake /home/richie/dotfiles#$HOST   --option substituters 'https://nix-community.cachix.org' --option trusted-public-keys 'cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY='";
+      "rebuild_backup" =
+        "sudo nixos-rebuild switch --flake /home/richie/dotfiles#$HOST   --option substituters 'https://nix-community.cachix.org' --option trusted-public-keys 'cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY='";
       "nix-test" = "nixos-rebuild test --flake /home/richie/dotfiles";
     };
   };
