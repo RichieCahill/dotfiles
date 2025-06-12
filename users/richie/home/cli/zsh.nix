938362cd2dd452fc434c966a3b7a9f4577661e2a
Richie Cahill <Richie@tmmworkshop.com>
2025-06-11 22:15:38 -0400

    updated shellAliases

--- users/richie/home/cli/zsh.nix
+++ users/richie/home/cli/zsh.nix
@@ -23,11 +23,9 @@
       "ll" = "eza --long --group";
       "la" = "eza --all";
 
-      "rspace" = "'for f in *\ *; do mv \"$f\" \"\${f// /_}\"; done'";
-      "rebuild" = "sudo nixos-rebuild switch --flake /home/richie/dotfiles#$HOST";
+      "rebuild" = "sudo nixos-rebuild switch --flake $HOME/dotfiles#$HOST";
       "rebuild_backup" =
-        "sudo nixos-rebuild switch --flake /home/richie/dotfiles#$HOST   --option substituters 'https://nix-community.cachix.org' --option trusted-public-keys 'cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY='";
-      "nix-test" = "nixos-rebuild test --flake /home/richie/dotfiles";
+        "sudo nixos-rebuild switch --flake $HOME/dotfiles#$HOST --option substituters 'https://nix-community.cachix.org' --option trusted-public-keys 'cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY='";
     };
   };
 }
