2dffb152864360ac9812ccefce1cc10142a40d2f
Richie Cahill <Richie@tmmworkshop.com>
2025-09-01 12:57:32 -0400

    updated loadModels and colsed open-webui firewall

--- systems/rhapsody-in-green/llms.nix
+++ systems/rhapsody-in-green/llms.nix
@@ -7,19 +7,18 @@
       loadModels = [
         "codellama:7b"
         "deepseek-r1:14b"
-        # "deepseek-r1:32b"
+        "deepseek-r1:32b"
         "deepseek-r1:8b"
         "gemma3:12b"
-        # "gemma3:27b"
-        # "gpt-oss:120b"
+        "gemma3:27b"
+        "gpt-oss:120b"
         "gpt-oss:20b"
-        # "qwen3:14b"
-        # "qwen3:30b"
+        "qwen3:14b"
+        "qwen3:30b"
       ];
     };
     open-webui = {
       enable = true;
-      openFirewall = true;
       host = "0.0.0.0";
       environment = {
         ANONYMIZED_TELEMETRY = "False";
