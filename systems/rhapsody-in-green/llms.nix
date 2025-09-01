a7b336a7ded4c32639b962a95914e1b28c328d9c
Richie Cahill <Richie@tmmworkshop.com>
2025-09-01 12:35:53 -0400

    adding llms to rhapsody-in-green

---
+++ systems/rhapsody-in-green/llms.nix
@@ -0,0 +1,45 @@
+{
+  services = {
+    ollama = {
+      user = "ollama";
+      enable = true;
+      host = "0.0.0.0";
+      loadModels = [
+        "codellama:7b"
+        "deepseek-r1:14b"
+        # "deepseek-r1:32b"
+        "deepseek-r1:8b"
+        "gemma3:12b"
+        # "gemma3:27b"
+        # "gpt-oss:120b"
+        "gpt-oss:20b"
+        # "qwen3:14b"
+        # "qwen3:30b"
+      ];
+    };
+    open-webui = {
+      enable = true;
+      openFirewall = true;
+      host = "0.0.0.0";
+      environment = {
+        ANONYMIZED_TELEMETRY = "False";
+        DO_NOT_TRACK = "True";
+        SCARF_NO_ANALYTICS = "True";
+        OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+        WEBUI_AUTH = "False";
+      };
+    };
+  };
+  systemd.services = {
+    ollama.serviceConfig = {
+      Nice = 19;
+      IOSchedulingPriority = 7;
+    };
+    ollama-model-loader.serviceConfig = {
+      Nice = 19;
+      CPUWeight = 50;
+      IOSchedulingClass = "idle";
+      IOSchedulingPriority = 7;
+    };
+  };
+}
