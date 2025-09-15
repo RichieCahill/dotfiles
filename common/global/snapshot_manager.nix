bff561946fcc1241aca89d4893e5ca1b3aae4753
Richie Cahill <Richie@tmmworkshop.com>
2025-09-15 00:25:43 -0400

    reviewed coderabbit

--- common/global/snapshot_manager.nix
+++ common/global/snapshot_manager.nix
@@ -18,7 +18,7 @@ in
         description = "Path to the snapshot_manager TOML config.";
       };
       EnvironmentFile = lib.mkOption {
-        type = lib.types.nullOr (lib.types.coercedTo lib.types.path lib.isString lib.types.str);
+        type = lib.types.nullOr (lib.types.coercedTo lib.types.path toString lib.types.str);
         default = null;
         description = ''
           Single environment file for the service (e.g. /etc/snapshot-manager/env).
@@ -37,7 +37,9 @@ in
         path = [ pkgs.zfs ];
         serviceConfig = {
           Type = "oneshot";
-          ExecStart = "${inputs.system_tools.packages.x86_64-linux.default}/bin/snapshot_manager --config-file=${lib.escapeShellArg cfg.path}";
+          ExecStart = "${
+            inputs.system_tools.packages.${pkgs.system}.default
+          }/bin/snapshot_manager --config-file=${lib.escapeShellArg cfg.path}";
         }
         // lib.optionalAttrs (cfg.EnvironmentFile != null) {
           EnvironmentFile = cfg.EnvironmentFile;
