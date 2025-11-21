c09e289c6cfae6d6dd8076d097220b3ab322e908
Richie Cahill <Richie@tmmworkshop.com>
2025-11-20 21:33:05 -0500

    adding udev rules

--- systems/rhapsody-in-green/qmk.nix
+++ systems/rhapsody-in-green/qmk.nix
@@ -9,6 +9,18 @@
     udev = {
       packages = [ pkgs.qmk-udev-rules ];
       extraRules = ''
+        # Keychron keyboards
+        KERNEL=="hidraw*", ATTRS{idVendor}=="3434", MODE="0660", GROUP="plugdev"
+        SUBSYSTEM=="usb",  ATTR{idVendor}=="3434", MODE="0660", GROUP="plugdev"
+
+        # Some boards use 32f0 as vendor id
+        KERNEL=="hidraw*", ATTRS{idVendor}=="32f0", MODE="0660", GROUP="plugdev"
+        SUBSYSTEM=="usb",  ATTR{idVendor}=="32f0", MODE="0660", GROUP="plugdev"
+
+        # Keychron HID device permissions
+        SUBSYSTEM=="usb", ATTR{idVendor}=="3434", MODE="0666"
+        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", MODE="0666"
+
         # Keychron / QMK common bootloaders
         SUBSYSTEM=="usb", ATTR{idVendor}=="0483", ATTR{idProduct}=="df11", MODE="0666", GROUP="plugdev"
         SUBSYSTEM=="usb", ATTR{idVendor}=="03eb", MODE="0666", GROUP="plugdev"
