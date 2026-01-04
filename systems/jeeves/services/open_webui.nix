203bdfe89b558e76937c66b2e16cee4465f84161
Richie Cahill <Richie@tmmworkshop.com>
2026-01-03 19:37:37 -0500

    added env vars

--- systems/jeeves/services/open_webui.nix
+++ systems/jeeves/services/open_webui.nix
@@ -6,5 +6,11 @@ in
     stateDir = "${vars.services}/open_webui/";
     enable = true;
     openFirewall = true;
+    environment = {
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+    };
   };
 }
