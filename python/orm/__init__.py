78b14cb94eb381a433b2270756640f4e7ac8dd88
Richie Cahill <Richie@tmmworkshop.com>
2026-01-10 13:54:54 -0500

    base of sqlalchemy alembic

---
+++ python/orm/__init__.py
@@ -0,0 +1,8 @@
+"""ORM package exports."""
+
+from __future__ import annotations
+
+from python.orm.base import RichieBase, TableBase
+from python.orm.temp import Temp
+
+__all__ = ["RichieBase", "TableBase", "Temp"]
