f627a5ac6e2bb43d45c22db75db5b95c1ca80fa7
Richie Cahill <Richie@tmmworkshop.com>
2026-03-26 08:13:39 -0400

    enabling kafka

--- systems/jeeves/services/kafka.nix
+++ systems/jeeves/services/kafka.nix
@@ -3,10 +3,17 @@ let
 in
 {
   services.apache-kafka = {
-    enable = false;
+    enable = true;
     settings = {
       listeners = [ "PLAINTEXT://localhost:9092" ];
       "log.dirs" = [ vars.kafka ];
+      "num.partitions" = 6;
+      "default.replication.factor" = 1;
+      "log.retention.hours" = 168;
+      "log.retention.bytes" = 10737418240;
+      "log.segment.bytes" = 1073741824;
+      "log.cleanup.policy" = "delete";
+      "auto.create.topics.enable" = false;
     };
   };
 }
