e368402eea7d1fe3768cd0e009295e809ac59953
Richie Cahill <Richie@tmmworkshop.com>
2026-03-29 12:02:00 -0400

    adding LegislatorSocialMedia

--- python/orm/data_science_dev/congress/__init__.py
+++ python/orm/data_science_dev/congress/__init__.py
@@ -1,13 +1,14 @@
 """init."""
 
 from python.orm.data_science_dev.congress.bill import Bill, BillText
-from python.orm.data_science_dev.congress.legislator import Legislator
+from python.orm.data_science_dev.congress.legislator import Legislator, LegislatorSocialMedia
 from python.orm.data_science_dev.congress.vote import Vote, VoteRecord
 
 __all__ = [
     "Bill",
     "BillText",
     "Legislator",
+    "LegislatorSocialMedia",
     "Vote",
     "VoteRecord",
 ]
